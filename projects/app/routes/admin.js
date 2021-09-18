a8eb886400d32de62626811f2f395e95e63173a3
DuarteB <eduarte.dev@gmail.com>
2021-09-17 22:05:17 -0300

    Mantendo o preenchimento dos campos de cadastro

--- projects/app/routes/admin.js
+++ projects/app/routes/admin.js
@@ -1,31 +1,30 @@
-module.exports = function(app) {
-    app.get('/formulario_inclusao_noticia', function(req, res){
-        res.render('admin/form_add_noticia')
-    })
+module.exports = function(app){
+    app.get('/formulario_inclusao_noticia', function(req,res){
+        res.render('admin/form_add_noticia', { validacao : {}, noticia : {} });
+    });
 
-    app.post('/noticias/salvar', function(req, res){
+    app.post('/noticias/salvar', function(req,res){
         var noticia = req.body;
 
         req.assert('titulo', 'Título é obrigatório').notEmpty();
         req.assert('resumo', 'Resumo é obrigatório').notEmpty();
-        req.assert('resumo', 'Resumo deve conter entre 10 a 100 caracteres').len(10, 100);
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
         req.assert('autor', 'Autor é obrigatório').notEmpty();
-        req.assert('data_noticia', 'Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'});;
-        req.assert('noticia', 'Noticia é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
 
-        var error = req.validationError();
+        var erros = req.validationErrors();
 
-        if(error){
-            res.render('admin/form_add_noticia')
+        if(erros){
+            res.render('admin/form_add_noticia', {validacao: erros,  noticia: noticia});
             return;
         }
 
         var connection = app.config.dbConnection();
-
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
-        noticiasModel.salvarNoticia(noticia, function(error, result) {
-            res.redirect('/noticias')
-        })
-    })
+        noticiasModel.salvarNoticia(noticia, function(error, result){
+            res.redirect('/noticias');
+        });
+    });
 }
--- projects/config/server.js
+++ projects/config/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const consign = require('consign');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
+var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
-const app = express();
+var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(expressValidator.check());
+app.use(expressValidator());
 
 consign()
     .include('app/routes')
