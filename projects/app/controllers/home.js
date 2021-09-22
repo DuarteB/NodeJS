921a44563cf96dbf284135e257313d65cd9cac9a
DuarteB <eduarte.dev@gmail.com>
2021-09-21 21:39:52 -0300

    Atualizando as views e incluindo recursos estáticos

--- projects/app/controllers/home.js
+++ projects/app/controllers/home.js
@@ -1,3 +1,9 @@
 module.exports.index = function(app, req, res) {
-    res.send('home/index')
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new  app.app.models.noticiasDAO(connection);
+
+    noticiasModel.get5UltimasNoticias(function(error, result) {
+        res.render('home/index', {noticias : result})
+    })
 }
--- projects/app/routes/home.js
+++ projects/app/routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
     app.get('/', function(req, res){
-        app.app.controllers.home.home(app, req, res);
+        app.app.controllers.home.index(app, req, res);
     })
 }
